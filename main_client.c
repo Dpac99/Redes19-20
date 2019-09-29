0493732989ab1ed213caf27fc01e14a8ab6c2d22
Andreia <andreia.sofia.pereira@tecnico.ulisboa.pt>
2019-09-30 00:21:12 +0100

    The 'register userID' is halfway there.

--- main_client.c
+++ main_client.c
@@ -9,36 +9,40 @@
 #include <stdio.h>
 
 //Commands
-#define REGISTER 				 "REG"
-#define REGISTER_RESPONSE 		 "RGR"
-#define TOPIC_LIST 				 "LTP"
-#define TOPIC_LIST_RESPONSE 	 "LTR"
-#define TOPIC_PROPOSE 			 "PTP"
-#define TOPIC_PROPOSE_RESPONSE 	 "PTR"
-#define QUESTION_LIST 			 "LQU"
-#define QUESTION_LIST_RESPONSE 	 "LQR"
-#define GET_QUESTION 			 "GQU"
-#define GET_QUESTION_RESPONSE 	 "QGR"
-#define SUBMIT_QUESTION 		 "QUS"
-#define SUBMIT_QUESTION_RESPONSE "QUR"
-#define SUBMIT_ANSWER 			 "ANS"
-#define SUBMIT_ANSWER_RESPONSE 	 "ANR"
-#define OK 						 "OK"
-#define NOK 					 "NOK"
-#define DUP 					 "DUP"
-#define ERROR 					 "ERR"
-#define END_OF_FILE 			 "EOF"
+#define REGISTER 				 "REG "
+#define REGISTER_RESPONSE 		 "RGR "
+#define TOPIC_LIST 				 "LTP " 
+#define TOPIC_LIST_RESPONSE 	 "LTR "
+#define TOPIC_PROPOSE 			 "PTP "
+#define TOPIC_PROPOSE_RESPONSE 	 "PTR "
+#define QUESTION_LIST 			 "LQU "
+#define QUESTION_LIST_RESPONSE 	 "LQR "
+#define GET_QUESTION 			 "GQU "
+#define GET_QUESTION_RESPONSE 	 "QGR "
+#define SUBMIT_QUESTION 		 "QUS "
+#define SUBMIT_QUESTION_RESPONSE "QUR "
+#define SUBMIT_ANSWER 			 "ANS "
+#define SUBMIT_ANSWER_RESPONSE 	 "ANR "
+#define OK 						 "OK "
+#define NOK 					 "NOK " 
+#define DUP 					 "DUP "
+#define ERROR 					 "ERR "
+#define END_OF_FILE 			 "EOF "
+
+#define BUFFER_SIZE				 128
 
 
 struct User{
-	char userId[5];
-	char selected_topic[10];		
-	char selected_question[10];
+	int userId;
+	char *selected_topic;		
+	char *selected_question;
 };
 
 void parseArgs(int argc, char *argv[], char *port, char *server_IP);
 int readCommand(char *buffer);
-void registerUser(char *buffer, int fd, struct addrinfo *res);
+struct User* initUser();
+void sendUDP(char *buffer, int fd, struct addrinfo *res, struct sockaddr_in addr);
+void registerUser(char *buffer, struct User *user);
 void topicList(char *buffer, int fd, struct addrinfo *res);
 void topicSelect(char *buffer, int flag);
 void topicPropose(char *buffer, int fd, struct addrinfo *res);
@@ -54,9 +58,11 @@ int main(int argc, char *argv[]){
 
 	struct addrinfo hints, *res;
 	struct sockaddr_in addr;
-	char buffer[256], *port, server_IP[128], command[64];
+	struct User *user = initUser();
+	char buffer[256], *port, *server_IP, command[64];
 
 	port = (char*)malloc(16);
+	server_IP = (char*)malloc(128);
 
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_family=AF_INET;      // IPv4
@@ -64,11 +70,11 @@ int main(int argc, char *argv[]){
 	hints.ai_flags=AI_NUMERICSERV;
 
 	parseArgs(argc, argv, port, server_IP);
+	if(strlen(server_IP)==0) server_IP = NULL;
 
 	n=getaddrinfo(server_IP, port, &hints, &res);
 	if(n!=0){
-		write(1, server_IP, strlen(server_IP));
-		write(1, port, strlen(port));
+		printf("0");
         exit(1);
     }
 
@@ -85,7 +91,9 @@ int main(int argc, char *argv[]){
 	while (strcmp(command, "exit") != 0){
 
 		if((strcmp(command, "register") == 0)|| (strcmp(command, "reg") == 0)){
-			registerUser(buffer, udp_fd, res);
+			registerUser(buffer, user);
+			sendUDP(buffer, udp_fd, res, addr);
+
 		}
 
 		else if((strcmp(command, "topic_list") == 0)|| (strcmp(command, "tl") == 0)){
@@ -128,22 +136,23 @@ int main(int argc, char *argv[]){
 			printf("Invalid command.\n");
 		}
 
+		/*memset(buffer, 0, sizeof(char));
         nwrite=sendto(udp_fd, buffer, size, 0, res->ai_addr, res->ai_addrlen);
         if(nwrite==-1){
 			printf("3");
             exit(1);
-        }
+        }*/
 		
 		memset(buffer, 0, sizeof(char));
         addrlen=sizeof(addr);
 
-        nread=recvfrom(udp_fd,buffer, 128, 0, (struct sockaddr*) &addr, &addrlen);
+        nread=recvfrom(udp_fd,buffer, BUFFER_SIZE, 0, (struct sockaddr*) &addr, &addrlen);
         if(nread==-1){
 			printf("4");
             exit(1);
         }
 	
-        write(1, "received: ", 6);
+        printf("Received: ");
 		write(1, buffer, nread);
 		printf("\n");
 
@@ -170,6 +179,7 @@ void parseArgs(int argc, char *argv[], char *port, char *server_IP){
         }  
     }
 	if(strlen(port)== 0) strcpy(port, "58053");
+	
 	return;
 }
 
@@ -186,7 +196,54 @@ int readCommand(char *buffer){
 	return i;
 }
 
-void registerUser(char *buffer, int fd, struct addrinfo *res){
+void sendUDP(char *buffer, int fd, struct addrinfo *res, struct sockaddr_in addr){
+	int nwrite, nread, size;
+
+	size = strlen(buffer);
+
+	nwrite=sendto(fd, buffer, size, 0, res->ai_addr, res->ai_addrlen);
+    if(nwrite==-1){
+		exit(1);
+	}
+	return;
+}
+
+struct User *initUser(){
+	struct User *user = (struct User *)malloc(sizeof(struct User));
+	user->userId = 0;
+	user->selected_topic = (char*)malloc(10 * sizeof(char));
+	user->selected_question = (char*)malloc(10 * sizeof(char));
+
+	return user;
+}
+
+void registerUser(char *buffer, struct User *user){
+	char *token;
+	int id, count = 0, n;
+	token = strtok(buffer, " ");
+	id = atoi(token);
+	n = id;
+	
+	if(id == 0){
+		printf("Invalid command format.\n");
+		return;
+	}
+    while(n != 0){
+        n /= 10;
+        ++count;
+    }
+	if(count != 5){
+		printf("Invalid command format.\n");
+		return;
+	}
+	if((token = strtok(NULL, " ")) != NULL){
+		printf("Invalid command format.\n");
+		return;
+	}
+
+	memset(buffer, 0, sizeof(char));
+	sprintf(buffer, "%s %d", REGISTER, id);
+	return;
 }
 
 void topicList(char *buffer, int fd, struct addrinfo *res){
